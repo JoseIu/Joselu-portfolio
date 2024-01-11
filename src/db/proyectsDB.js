6f6c21791cecd6af2a29ab5bdeafd75d9ad7db90
Joselu <joseluissgz97@gmail.com>
2024-01-11 11:49:58 +0100

    proyectsDB

---
+++ src/db/proyectsDB.js
@@ -0,0 +1,52 @@
+export const PROYECTS = [
+  {
+    id: "1",
+    img: "/proyects/mab-box2.webp",
+    title: "Map-Box",
+    tegnologies: ["Angular", "TypeScript", "HTML", "SCSS"],
+    description:
+      "Durante mi tiempo trabajando en el proyecto Map-box Demo con Angular 16, logré aplicar y poner en práctica de manera efectiva conceptos avanzados como standalone components, signals y la estrategia de componentización. Este proyecto se centró en la potenciación de la API de Map-box, donde mi objetivo era incorporar funcionalidades, tales como la implementación de características como la adición de marcadores, personalización de los mismos y la capacidad de desplazarlos por el mapa, manteniendo los datos persistentes.",
+    demo: "https://mapbox-app-lemon.vercel.app/maps/markers",
+    repo: "https://github.com/JoseIu/mapbox-app",
+  },
+  {
+    id: "2",
+    img: "/proyects/al-andalus.webp",
+    title: "Al-Andalus Doner Kebab",
+    tegnologies: ["HTML", "CSS", "JS"],
+    description:
+      "Durante la ejecución del proyecto Al-Andalus Doner Kebab Demo, empleé HTML, JavaScript y Sass para desarrollar una página web con un enfoque 'Mobile First', asegurando así su adaptabilidad a distintos dispositivos. Para estructurar el código de manera eficiente y mantenible, implementé la metodología BEM. Uno de los objetivos clave fue la creación de un menú interactivo que abarcara todas las opciones de la carta, proporcionando a los usuarios una experiencia completa y accesible. Este proyecto representó un compromiso integral con el diseño responsivo y las prácticas de desarrollo que garantizan una interfaz de usuario efectiva y atractiva.",
+    demo: "https://al-andalus-doner-kebab.netlify.app/",
+    repo: "https://github.com/JoseIu/al-andalus-kebab.github.io",
+  },
+  {
+    id: "3",
+    img: "/proyects/users-filter.webp",
+    title: "Users Filter",
+    tegnologies: ["Angular 17", "TypeScript", "HTML", "SCSS"],
+    description:
+      "Desarrollé un filtro de usuarios que permite la clasificación por activos o inactivos. Los usuarios pueden desactivar o activar el filtro, incluso con filtros específicos en funcionamiento, proporcionando una experiencia versátil y fácil de usar",
+    demo: "https://mapbox-app-lemon.vercel.app/maps/markers",
+    repo: "https://github.com/JoseIu/mapbox-app",
+  },
+  {
+    id: "4",
+    img: "/proyects/search-gifs.webp",
+    title: "Buscador de gifs",
+    tegnologies: ["Angular 16", "TypeScript", "HTML", "SCSS"],
+    description:
+      "En la creación de este proyecto, utilicé Angular 16, HTML, SASS y TypeScript, empleando específicamente Angular's HttpClient para consumir eficientemente la API de Giphy a través de peticiones HTTP. Destaco tambien la implementación de un historial de búsquedas, permitiendo a los usuarios repetir búsquedas anteriores con un clic. Aseguré la eficiencia del historial al evitar duplicación de nombres. Este proyecto resalta mi capacidad para desarrollar aplicaciones interactivas y mi habilidad en el uso de herramientas avanzadas como Angular's HttpClient para optimizar la comunicación con la API.",
+    demo: "https://github.com/JoseIu/Angular-searchGifs",
+    repo: "https://angular-search-gifs.vercel.app/",
+  },
+  {
+    id: "5",
+    img: "/proyects/recetas.webp",
+    title: "Buscador de recetas",
+    tegnologies: ["HTML", "Sass", "JavaScript"],
+    description:
+      "Desarrollé una aplicación para buscar recetas que incorpora un modal para visualizar los ingredientes. Además, los usuarios tienen la capacidad de añadir recetas a la lista de favoritos, proporcionando una experiencia interactiva y funcional. Este proyecto destaca mi habilidad para trabajar con tecnologías web clave y crear aplicaciones prácticas y atractivas.",
+    demo: "https://buscad0r-recetas-api.netlify.app/",
+    repo: "https://github.com/JoseIu/buscador_recetas_API",
+  },
+];
