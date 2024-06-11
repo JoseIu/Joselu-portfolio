bbe7cb3af6e12de513a24d51d785794401bc6dff
Joselu <joseluissgz97@gmail.com>
2024-06-11 18:49:15 +0200

    add: oxygen-shop image and data

--- src/db/proyectsDB.js
+++ src/db/proyectsDB.js
@@ -139,6 +139,16 @@ export const PROYECTS_REACT = [
 	},
 	{
 		id: uuidv4(),
+		img: "/react/oxygen-shop.webp",
+		title: "Oxygen Shop",
+		tegnologies: ["HTML", "CSS", "SASS", "JavaScript"],
+		description:
+			"Página web para Oxygen Shop, una tienda de ropa. En este proyecto, utilicé HTML, CSS y JavaScript para desarrollar una interfaz de usuario atractiva y funcional. Implementé un diseño responsivo y atractivo, asegurando que la página fuera accesible en distintos dispositivos. Además, utilicé SASS y la metodología BEM para estructurar el código de manera eficiente y mantenible. Este proyecto me permitió demostrar mi habilidad para crear interfaces de usuario efectivas y atractivas.",
+		demo: "https://joseiu.github.io/Oxygen-shop/",
+		repo: "https://github.com/JoseIu/Oxygen-shop",
+	},
+	{
+		id: uuidv4(),
 		img: "/react/al-andalus.webp",
 		title: "Al-Andalus Doner Kebab",
 		tegnologies: ["HTML", "CSS", "JS"],
